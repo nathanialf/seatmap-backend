3f22b462b5b9d29fea07800af9c1a9e0be0c3e51
Nathanial Fine <nathanial.fine@gmail.com>
2025-11-24 19:41:53 +0000

    Remove dev.myseatmap.com NS delegation record
    Remove unnecessary NS delegation record for dev.myseatmap.com subdomain from prod account configuration.

--- terraform/environments/prod/domain.tf
+++ terraform/environments/prod/domain.tf
@@ -21,18 +21,3 @@ resource "aws_route53_record" "api_dev_ns" {
     "ns-628.awsdns-14.net"
   ]
 }
-
-# NS Record for dev.myseatmap.com subdomain delegation
-resource "aws_route53_record" "dev_ns" {
-  zone_id = aws_route53_zone.myseatmap.zone_id
-  name    = "dev.myseatmap.com"
-  type    = "NS"
-  ttl     = 300
-
-  records = [
-    "ns-977.awsdns-58.net",
-    "ns-1865.awsdns-41.co.uk",
-    "ns-1121.awsdns-12.org",
-    "ns-320.awsdns-40.com"
-  ]
-}
